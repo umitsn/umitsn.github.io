3899a7e13a47b8280a75833e2caed046f2fa746b
Ümit Şen <49841290+umitsn@users.noreply.github.com>
2024-07-24 12:36:06 +0300

    Add files via upload

---
+++ site/script.js
@@ -0,0 +1,16 @@
+
+const contentEl = document.getElementById("content")
+
+const LoadPage = (page) => {
+    fetch(`${page}.html`).then(
+        res => {
+            if (res.ok) {
+                return res.text()
+            }
+        }
+    ).then(
+        htmlSnippet => {
+            contentEl.innerHTML = htmlSnippet
+        }
+    )
+}
