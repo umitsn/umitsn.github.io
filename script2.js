89904068a866644c15393148249f80ff13b2ee67
Ümit Şen <49841290+umitsn@users.noreply.github.com>
2025-07-14 00:09:10 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,13 +1,31 @@
 let allVideos = [
   {
     id: "nhhsmV03knU",
-    title: "split() Fonksiyonu - 1 Dakikada Python",
+    title: "0 Fonksiyonu - 1 Dakikada Python",
     description: "split() fonksiyonu, bir string'i boşluk veya belirli bir ayıraca göre parçalara ayırır.",
     code: "text = \"Merhaba dünya\"\nkelimeler = text.split()\nprint(kelimeler)  # ['Merhaba', 'dünya']"
   },
   {
-    id: "yWpR63iUcMI",
-    title: "join() Fonksiyonu - Listeyi Stringe Çevir",
+    id: "nhhsmV03knU",
+    title: "1 Fonksiyonu - Listeyi Stringe Çevir",
+    description: "join() fonksiyonu, bir listeyi belirli bir ayırıcı ile birleştirerek string oluşturur.",
+    code: "kelimeler = ['Python', 'çok', 'güçlü']\ncumle = ' '.join(kelimeler)\nprint(cumle)  # Python çok güçlü"
+  },
+  {
+    id: "nhhsmV03knU",
+    title: "2 Fonksiyonu - Listeyi Stringe Çevir",
+    description: "join() fonksiyonu, bir listeyi belirli bir ayırıcı ile birleştirerek string oluşturur.",
+    code: "kelimeler = ['Python', 'çok', 'güçlü']\ncumle = ' '.join(kelimeler)\nprint(cumle)  # Python çok güçlü"
+  },
+  {
+    id: "nhhsmV03knU",
+    title: "3 Fonksiyonu - Listeyi Stringe Çevir",
+    description: "join() fonksiyonu, bir listeyi belirli bir ayırıcı ile birleştirerek string oluşturur.",
+    code: "kelimeler = ['Python', 'çok', 'güçlü']\ncumle = ' '.join(kelimeler)\nprint(cumle)  # Python çok güçlü"
+  },
+  {
+    id: "nhhsmV03knU",
+    title: "4 Fonksiyonu - Listeyi Stringe Çevir",
     description: "join() fonksiyonu, bir listeyi belirli bir ayırıcı ile birleştirerek string oluşturur.",
     code: "kelimeler = ['Python', 'çok', 'güçlü']\ncumle = ' '.join(kelimeler)\nprint(cumle)  # Python çok güçlü"
   }
