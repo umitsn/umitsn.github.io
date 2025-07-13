abc398405c66ca41b572e494f885b8be10714513
Ümit Şen <49841290+umitsn@users.noreply.github.com>
2025-07-13 23:11:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,22 @@ const LoadPage = (page) => {
         }
     ).then(
         htmlSnippet => {
+            contentEl.innerHTML = htmlSnippet;
+            const contentEl = document.getElementById("content")
+
+const LoadPage = (page) => {
+    fetch(`${page}.html`).then(
+        res => {
+            if (res.ok) {
+                return res.text()
+            }
+        }
+    ).then(
+        htmlSnippet => {
             contentEl.innerHTML = htmlSnippet
+        }
+    )
+}
         }
     )
 }
